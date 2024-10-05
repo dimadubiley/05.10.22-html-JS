e81baedda0ed4953da361484d9d4108cf0d41c64
MisterNP <dimon.pr.2007@gmail.com>
2024-10-05 22:14:19 +0300

    first commit

---
+++ src/scripts.js
@@ -0,0 +1,14 @@
+let blue = document.getElementById("blue");
+        let blue1 = document.getElementById("blue1");
+        let blue2 = document.getElementById("blue2");
+        let blue3 = document.getElementById("blue3");
+        let notBlue = document.getElementById("notBlue");
+
+        let bl = "blue"
+
+        // Применение стилей к элементам
+        blue.style.background = bl;
+        blue1.style.background = bl;
+        blue2.style.background = bl;
+        blue3.style.background = bl;
+        notBlue.style.background = "pink";
